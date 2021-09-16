d2852f958925e83a14a7506b165fa10ebb5ee1d4
abbasShDev <abbas20alzaeem@gmail.com>
2021-09-16 11:23:50 +0300

    Prepare for deployment

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-
-const CONNECTION_URL =
-  "mongodb+srv://abbasshdev:abbasshdev537823389@cluster0.xhavj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
