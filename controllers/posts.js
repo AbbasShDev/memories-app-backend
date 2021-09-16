892d2790d17fa65bf971039ba8881b8ad0178896
abbasShDev <abbas20alzaeem@gmail.com>
2021-09-16 10:05:24 +0300

    Delete post done

--- controllers/posts.js
+++ controllers/posts.js
@@ -44,3 +44,18 @@ export const updatePost = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ error: "Post not found" });
+
+  try {
+    await Post.findByIdAndRemove(id);
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
