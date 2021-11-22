7cba0453e5658c229fd7dc2d911c150038c541ce
abbasShDev <abbas20alzaeem@gmail.com>
2021-11-22 12:41:11 +0300

    Adding pagination

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,10 +2,23 @@ import mongoose from "mongoose";
 import Post from "../models/Post.js";
 
 export const getPosts = async (req, res) => {
-  try {
-    const posts = await Post.find({}).sort({ createdAt: -1 });
+  const { page } = req.query;
 
-    res.status(200).json(posts);
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT;
+    const total = await Post.countDocuments({});
+
+    const posts = await Post.find({})
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
@@ -13,6 +26,7 @@ export const getPosts = async (req, res) => {
 
 export const getPostsBySearch = async (req, res) => {
   const { searchQuery, tags } = req.query;
+
   try {
     const title = new RegExp(searchQuery, "i");
 
